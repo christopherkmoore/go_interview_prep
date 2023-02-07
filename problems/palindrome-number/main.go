ff9768a661013805799f7fbe46c5629286ae7d47
Christopher Moore <christopherkmoore1@gmail.com>
2023-02-06 16:35:27 -0800

    adds solution without using string manipulation

--- problems/palindrome-number/main.go
+++ problems/palindrome-number/main.go
@@ -26,9 +26,7 @@ func main() {
 }
 
 // simple solution, conver the number to a string and evaluate the characters
-
-// without strings: ?
-// []int split, reverse, join == x?
+// CKM -- passing 94 MS
 func isPalindrome(x int) bool {
 	// input < 10 == true && input > 0 false
 	if x < 0 {
@@ -59,3 +57,70 @@ func isPalindrome(x int) bool {
 }
 
 // without strings: x % 10
+
+// without strings
+// CKM - passing 94MS
+// func isPalindrome(x int) bool {
+
+// 	if x < 0 {
+// 		return false
+// 	}
+
+// 	if x > 0 && x < 10 {
+// 		return true
+// 	}
+
+// 	base := 1
+// 	incrementor := 10
+// 	check := base * incrementor
+
+// 	last := 0
+// 	iterator := getIterator(x)
+
+// 	var reversed []int
+// 	for i := 0; i < iterator; i++ {
+// 		last = x % check
+// 		reverse := float64(base) / (float64(check) / float64(incrementor))
+
+// 		digit := int(float64(last) * reverse)
+
+// 		reversed = append(reversed, digit)
+// 		check = check * incrementor // 10^1, 10^2 ... 10^n
+// 	}
+
+// 	for i := 0; i < iterator; i++ {
+// 		base = base * 10
+// 	}
+
+// 	var forwards []int
+// 	for i := 0; i < iterator; i++ {
+// 		check := x % base
+// 		reverse := float64(1) / (float64(base) / float64(incrementor))
+
+// 		digit := int(float64(check) * reverse)
+
+// 		forwards = append(forwards, digit)
+// 		base = base / incrementor // 10^1, 10^2 ... 10^n
+// 	}
+
+// 	return reflect.DeepEqual(reversed, forwards)
+// }
+
+// func getIterator(x int) int {
+// 	base := 1
+// 	incrementor := 10
+// 	check := base * incrementor
+
+// 	count := 0
+// 	for {
+// 		count++
+
+// 		if x%check == x {
+// 			break
+// 		}
+
+// 		check = check * incrementor // 10^1, 10^2 ... 10^n
+// 	}
+
+// 	return count
+// }
